6a73ac8e02d8035504323a2a4b795806edec5a31
nrennie <nrennie35@gmail.com>
2024-08-05 17:25:19 +0100

    Initializing gh-pages branch

---
+++ exercises/exercise_01.R
@@ -0,0 +1,24 @@
+
+# Install R packages ------------------------------------------------------
+
+pkgs <- c(
+  "tidymodels", "tidyverse", "glmnet", "vip",
+  "ranger", "kernlab", "ggplot2movies"
+)
+install.packages(pkgs)
+
+
+
+# Load the movies data --------------------------------------------
+
+# Subset columns to remove the `title`, `budget`, `mpaa`, and `r?` columns.
+
+# Create a binary outcome from the `rating` column.
+
+# Ensure all binary columns are factors.
+
+
+
+# Inspect variables -------------------------------------------------------
+
+
---
+++ exercises/exercise_02.R
@@ -0,0 +1,21 @@
+
+# Load {tidymodels} -------------------------------------------------------
+
+
+
+
+# Split into training and testing -----------------------------------------
+# Choose your own proportion for the split!
+
+
+# Create cross validation folds
+# Choose how many splits and how many repeats!
+
+
+# Build a recipe ----------------------------------------------------------
+
+# Use the `recipe()` function and the `step_*() functions`
+
+
+# create a workflow and add the recipe
+
